3e1174cb794e142505d34bdb2371adab6dbd7373
Georges034302 <georges.boughantous@gmail.com>
2021-02-23 20:09:40 +1100

    lab1.0

--- labs/lab01/lab1.js
+++ labs/lab01/lab1.js
@@ -1,32 +1,34 @@
+//Set the pixel ration of the Canvas resolution
+var ratio = window.innerWidth / window.innerHeight;
+
+//To display anything with three.js, we need three objects: scene, camera and renderer.
 var scene = new THREE.Scene();
-var ratio = window.innerWidth/window.innerHeight;
-var camera = new THREE.PerspectiveCamera(45,ratio,0.1,1000);
-
-camera.position.set(2,5,15);
-camera.lookAt(0,1,5);
-
+var camera = new THREE.PerspectiveCamera(45, ratio, 0.1, 1000);
+camera.position.set(2, 5, 15);
+camera.lookAt(0, 1, 5);
 var renderer = new THREE.WebGLRenderer();
-
-renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+//Create a cube from BoxGeometry and BasicMeshMaterial
 var cube_material = new THREE.MeshBasicMaterial();
-cube_material.color = new THREE.Color(0,1,0);
+cube_material.color = new THREE.Color(0, 1, 0);
 cube_material.wireframe = true;
-var cube_geo = new THREE.BoxGeometry(3,3,3);
-var cube = new THREE.Mesh(cube_geo,cube_material);
+var cube_geo = new THREE.BoxGeometry(3, 3, 3);
+var cube = new THREE.Mesh(cube_geo, cube_material);
 cube.position.x = -2;
 
+//Create a cube from SphereGeometry and BasicMeshMaterial
 var sphere_material = new THREE.MeshBasicMaterial();
-sphere_material.color = new THREE.Color(55,1,0);
+sphere_material.color = new THREE.Color(55, 1, 0);
 sphere_material.wireframe = true;
-var sphere_geo = new THREE.SphereGeometry(2,16,16);
-var sphere = new THREE.Mesh(sphere_geo,sphere_material);
+var sphere_geo = new THREE.SphereGeometry(2, 16, 16);
+var sphere = new THREE.Mesh(sphere_geo, sphere_material);
 sphere.position.x = 2;
 
-
-
+//Add the geometrical objects to the scene
 scene.add(sphere);
 scene.add(cube);
 
-renderer.render(scene,camera);
+//Render the scene and camera on the canvas
+renderer.render(scene, camera);
