795be5ccfca63b699829405757cd0fbcca1bc611
Georges034302 <georges.boughantous@gmail.com>
2021-03-02 11:20:36 +1100

    orbital control

--- labs/lab01/animate.js
+++ labs/lab01/animate.js
@@ -1,17 +1,17 @@
 //Animate a cube to rotate around: x, and y axis
 function animate_cube() {
     requestAnimationFrame(animate_cube);
-    cube.rotation.x += 0.04;
-    cube.rotation.y += 0.04;
+    cube.rotation.x = 0.04;
+    cube.rotation.y = 0.04;
     cube.position.z = 7;
     renderer.render(scene, camera);
 }
 
 //Animate a sphere to rotate around x axis and transform along y, z axis
 function animate_sphere() {
-    requestAnimationFrame(animate_sphere);
+    
     sphere.rotation.x = 0.04;
-    sphere.rotation.y += 0.05;
+    sphere.rotation.y = 0.05;
     sphere.position.z = 4;
 
     renderer.render(scene, camera);
