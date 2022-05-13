a2f20ea821114b16b875f231aecf35bd5d90dac5
Georges034302 <georges.boughantous@gmail.com>
2022-05-13 17:28:14 +1000

    lab11

--- labs/lab10/animate.js
+++ labs/lab10/animate.js
@@ -11,5 +11,18 @@ animate();
 
 //render and animate the cloud
 function spin() {
+    alpha += dalpha;
+    var alphas = cloud.geometry.attributes.alpha;
+    var count = alphas.count;
 
+    for (var i = 0; i < count; i++) {
+        alphas[i] *= 0.95;
+        if (alphas[i] < 0.01) {
+            alphas[i] = 1.0;
+        }
+    }
+    alphas.needsUpdate = true;
+    cloud.position.x = -r * Math.cos(alpha);
+    cloud.position.z = r * Math.sin(alpha);
+    renderer.render(scene, camera);
 }
