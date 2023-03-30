4e373da3c333f24e5d938f43c0bce1e8cefb4acc
Georges034302 <georges.boughantous@gmail.com>
2023-03-30 18:50:07 +1100

    lab6

--- labs/lab06/animate.js
+++ labs/lab06/animate.js
@@ -13,5 +13,24 @@ var selectedObj = false;
 
 //Define a function to select/drop a PLY object 
 function onDocumentMouseDown(event) {
+    var mouse = new THREE.Vector2();
+    mouse.x = (event.clientX/renderer.domElement.clientWidth) * 2 -1;
+    mouse.y = -(event.clientY/renderer.domElement.clientHeight) *2 -1;
 
+    rayCaster.setFromCamera(mouse,camera);
+    var intersects = rayCaster.intersectObjects(scene.children,false);
+
+    if(intersects.length > 0){
+        if(intersects[0].object.name === "loaded_mesh" && !selectedObj){
+            mesh.material.color = new THREE.Color(1,1,0.5);
+            selectedObj = true;
+        }
+        if(intersects[0].object.name === "loaded_mesh" && selectedObj){
+            mesh.material.color = new THREE.Color(0.6,0.2,0.4);
+            var pos = intersects[0].point;
+            mesh.position.x = pos.x;
+            mesh.position.y = pos.y;
+            selectedObj = false;
+        }
+    }
 }
