504fbe596ce81ef7663c5872701fd26fac995007
Georges034302 <georges.boughantous@gmail.com>
2023-04-27 12:14:17 +1000

    lab9

--- labs/lab09/setup.js
+++ labs/lab09/setup.js
@@ -1,74 +1,55 @@
 /* global THREE */
 //Declare Systen Variables
 var scene = new THREE.Scene();
-var renderer ;
+var renderer;
 var camera;
-var num_cam = 6;
-const cameras = [];
+var ratio = window.innerWidth / window.innerHeight;
 
-var width_cam = ( window.innerWidth / num_cam ) * window.devicePixelRatio;
-var height_cam = ( window.innerHeight / num_cam ) * window.devicePixelRatio;
-var ratio = window.innerWidth / window.innerHeight; 
+//Step 1: Declare the mirror variables
+//Define the number of cameras
+//Define an array of cameras
+//Define the cam width based on the window ratios and number of cameras
+//Define the cam height based on the window ratios and number of cameras
 
-//Setup the 3 main components: scene, camera, renderer
-function createCamera() {  	
-    for ( let y = 0; y < num_cam; y ++ ) {
-		for ( let x = 0; x < num_cam; x ++ ) {
-			const subcamera = new THREE.PerspectiveCamera( 45, ratio, 0.1, 1000 );
-			subcamera.viewport = new THREE.Vector4( Math.floor( x * width_cam ), 
-													Math.floor( y * height_cam ),
-												    Math.ceil( width_cam ), 
-													Math.ceil( height_cam ) );
 
-			subcamera.position.x = (x*5) - 0.5;
-			subcamera.position.x = (y*5) - 0.5;
-			subcamera.position.z = 4;
-			subcamera.position.multiplyScalar( 2 );
-			subcamera.lookAt( 0, 0, 0 );
-			subcamera.updateMatrixWorld();
-			cameras.push( subcamera );
-		}
-	}
-	var mycamera = new THREE.ArrayCamera( cameras );
-	mycamera.position.z = 3;
-    var cameralight = new THREE.PointLight( new THREE.Color(1,1,1), 1 );
-    mycamera.add( cameralight ); 		
-	return mycamera;	
+//Step2: Update this function to return an array of cameras
+function createCamera() {
+    //add a collections of cameras to the array
+    //specify the viewport for each camera in the collection
+    //specify the position for each camera in the collection
+
+    var mycamera = new THREE.PerspectiveCamera(45, ratio, 0.1, 1000);
+    mycamera.position.set(0, 0, 15);
+    mycamera.lookAt(0, 0, 1);
+    mycamera.position.z = 15;
+    var cameralight = new THREE.PointLight(new THREE.Color(1, 1, 1), 1);
+    mycamera.add(cameralight);
+    return mycamera;
 }
 camera = createCamera();
 
-function createRenderer(){
-	var myrenderer =  new THREE.WebGLRenderer({antialias:true});	
+function createRenderer() {
+    var myrenderer = new THREE.WebGLRenderer({ antialias: true });
     myrenderer.setSize(window.innerWidth, window.innerHeight);
-	document.body.appendChild(myrenderer.domElement);  
-	return myrenderer;	
+    document.body.appendChild(myrenderer.domElement);
+    return myrenderer;
 }
 renderer = createRenderer();
 
-//Resize the scene and update the camera aspect to the screen ration
+//Step 3: update this function to set the viewport for each camera in the array in the resize matrix
 var resizeScene = function() {
-	var width = window.innerWidth;
+    var width = window.innerWidth;
     var height = window.innerHeight;
-    ratio = width/height;
-	console.log("Ratio "+ratio); 
-    width_cam = ( width / num_cam ) * window.devicePixelRatio;
-	height_cam = ( height / num_cam ) * window.devicePixelRatio;
-		
-	camera.aspect = ratio;	
-	camera.updateProjectionMatrix();
-	
-	for ( let y = 0; y < num_cam; y ++ ){
-		for ( let x = 0; x < num_cam; x ++ ){
-			const subcamera = camera.cameras[ num_cam * y + x ];
-			subcamera.viewport.set(
-					Math.floor( x * width_cam ),
-					Math.floor( y * height_cam ),
-					Math.ceil( width_cam ),
-					Math.ceil( height_cam ) );
-			subcamera.aspect = ratio;
-			subcamera.updateProjectionMatrix();
-		}
-	}
-	renderer.setSize( window.innerWidth, window.innerHeight ); 
-};
+    ratio = width / height;
+    camera.aspect = ratio;
 
+    //Define the cam width based on the window ratios and number of cameras
+    //Define the cam height based on the window ratios and number of cameras
+    //specify the viewport for each camera in the collection
+    //specify the position for each camera in the collection
+    //set the aspect for each camera in the array
+    //update the projection matrix for each camera in the array
+
+    camera.updateProjectionMatrix();
+    renderer.setSize(width, height);
+};
