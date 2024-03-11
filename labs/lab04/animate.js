34949a78f2b95471c902e36eaf5cf6ddaa853bbd
Georges034302 <georges.boughantous@gmail.com>
2024-03-12 00:09:10 +1100

    Lab 4-starter

---
+++ labs/lab04/animate.js
@@ -0,0 +1,29 @@
+const speed = 0.003;
+
+//Animate a sphere to rotate around x axis and transform along y, z axis
+function animate_earth() {
+    requestAnimationFrame(animate_earth);
+    earth.rotation.y += speed;
+    renderer.render(scene, camera);
+}
+
+function animate_moons(){
+    requestAnimationFrame(animate_moons);
+    group.rotation.y += speed;
+    renderer.render(scene, camera);
+}
+
+function change_color(object){
+    var color = new THREE.Color(0xffffff);
+    color.setHex(Math.random()*0xffffff);
+    object.material.color = color;
+}
+
+function animate_color(){
+    renderer.render(scene, camera);
+    moons.forEach(change_color);
+    setTimeout(function(){
+        requestAnimationFrame(animate_color);
+    },100
+    );
+}
---
+++ labs/lab04/run.js
@@ -0,0 +1,9 @@
+setScene();
+
+addShapes();
+
+animate_earth();
+animate_moons();
+
+animate_color();
+window.addEventListener('resize', resizeScene);
