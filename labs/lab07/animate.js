7242ba9918c4266fd2a46de85aa3dcf9c8aaf012
Georges034302 <georges.boughantous@gmail.com>
2025-04-04 15:38:15 +1100

    lab7 sol

--- labs/lab07/animate.js
+++ labs/lab07/animate.js
@@ -14,6 +14,19 @@ var delta = 0;
 // The function is called in a loop to create the animation effect.
 function animate() {
     //code goes here
+    interval += velocity;
+    let pos = tissue.geometry.getAttribute('position');
+    for (let i = 0; i < pos.count; i++) {
+        let currentP = new THREE.Vector3();
+        currentP.x = pos.getX(i);
+        currentP.y = pos.getY(i);
+        currentP.z = pos.getZ(i);
+        let len = period * currentP.length() / size;
+        currentP.z = heightWave * Math.cos(len + interval);
+        pos.setZ(i, currentP.z);
+    }
+    pos.needsUpdate = true;
+    tissue.geometry.computeVertexNormals();
 }
 
 
@@ -23,6 +36,22 @@ var gui;
 // The color control allows the user to select a color, which updates the material_tissue color when changed.
 function buildGui() {
     //code goes here
+    gui = new dat.GUI();
+    var params = {
+        color: material_tissue.color.getHex(),
+        velocity_tissue: velocity,
+        heightWave: heightWave,
+    };
+    gui.addColor(params, 'color').onChange(function (e) {
+        material_tissue.color.setHex(e);
+    });
+    gui.add(params, 'velocity_tissue', 0, 1).onChange(function (e) {
+        velocity = e;
+    });
+    gui.add(params, 'heightWave', 0, 1).onChange(function (e) {
+        heightWave = e;
+    });
+    gui.open();
 }
 
 // The bounce function creates a bouncing effect for a 3D object by modifying its z-coordinate based on a sine function.
@@ -30,6 +59,9 @@ function buildGui() {
 // The object's z-coordinate oscillates between 1.5 and 3.5, creating a bouncing effect.
 function bounce(object) {
     //code goes here
+    delta = clock.getDelta();
+    time += delta;
+    object.position.z = 1.5 + Math.abs(Math.sin(time*3)) * 2;
 }
 
 //final update loop
