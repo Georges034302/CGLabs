51ce373e84e7cc60fd09f41ef1560ab50e2142bc
Georges034302 <georges.boughantous@gmail.com>
2025-05-09 16:53:43 +1000

    lab 9 -sol

--- labs/lab09/setup.js
+++ labs/lab09/setup.js
@@ -24,18 +24,29 @@ var ratio = window.innerWidth / window.innerHeight;
  * The ArrayCamera is then returned as the main camera for the scene.
  */
 function createCamera() {
-    // Create a single PerspectiveCamera
-    const singleCamera = new THREE.PerspectiveCamera(45, ratio, 0.1, 1000);
+    for (let y = 0; y < num_cam; y++) {
+        for (let x = 0; x < num_cam; x++) {
+            const subcamera = new THREE.PerspectiveCamera(45, ratio, 0.1, 1000);
+            subcamera.viewport = new THREE.Vector4(
+                Math.floor(x * width_cam),
+                Math.floor(y * height_cam),
+                Math.ceil(width_cam),
+                Math.ceil(height_cam)
+             );
 
-    // Position the camera further away
-    singleCamera.position.set(0, 0, 8); // Adjusted z position to zoom out
-    singleCamera.lookAt(0, 0, 0); // Make the camera look at the origin
-
-    // Add a point light to the camera
-    const cameralight = new THREE.PointLight(new THREE.Color(1, 1, 1), 1);
-    singleCamera.add(cameralight);
-
-    return singleCamera; // Return the single camera
+                subcamera.position.x = (x *5) - 0.05;
+                subcamera.position.y = (y *5) - 0.05;
+                subcamera.position.z = 4;
+                subcamera.position.multiplyScalar(2);
+                subcamera.lookAt(0, 0, 0);
+                subcamera.updateMatrixWorld();
+                cameras.push(subcamera);
+        }}
+    const arrayCamera = new THREE.ArrayCamera(cameras);
+    arrayCamera.position.z = 3;
+    var cameraLight = new THREE.PointLight(new THREE.Color(1, 1, 1), 1);
+    arrayCamera.add(cameraLight);
+    return arrayCamera;
 }
 
 /*
