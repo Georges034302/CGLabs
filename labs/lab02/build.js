c415ce3b9df8d96fc25dbcf4b44b2c414aef32ea
Georges034302 <georges.boughantous@gmail.com>
2026-02-24 14:19:22 +1100

    lab2

---
+++ labs/lab02/build.js
@@ -0,0 +1,34 @@
+/* Define the create sphere function
+ * - create a material object (type MeshBasicMaterial)
+ * - set the color for the material object (HEX)
+ * - set the wireframe for the material object to true
+ * - create the sphere geometry object (radius, hlines, vlines)
+ * - create a sphere object from the geometry and material
+ * - the function returns a sphere object
+ */
+function createSphere(radius, hlines, vlines, color) {
+    // code goes here
+    var material = new THREE.MeshBasicMaterial();
+    material.color = new THREE.Color(color);
+    material.wireframe = true;
+    var geometry = new THREE.SphereGeometry(radius,hlines,vlines);
+    var sphere = new THREE.Mesh(geometry,material);
+    return sphere;
+}
+
+/*
+ * Create earth sphere (radius=2,hlines=44,vlines=44,color=green)
+ * Create moon sphere (radius=1,hlines=32,vlines=32,color=white)
+ */
+var earth = createSphere(2,44,44,"#00ff00");
+var moon = createSphere(1,32,32,"#c0c0c0");
+
+/* Define the add shapes function
+ * - add the earth to the scene
+ * - add the moon to the scene
+ */
+function addShapes() {
+    // code goes here
+    scene.add(earth);
+    scene.add(moon);
+}
---
+++ labs/lab02/run.js
@@ -0,0 +1,12 @@
+/* Execute the graphics setup, build, and animate functions
+ * - setScene
+ * - addShapes
+ * - animate_earth
+ * - animate_moon
+ * - add window event listener to trigger the resize function
+ */
+setScene();
+addShapes();
+animate_earth();
+animate_moon();
+window.addEventListener('resize',resizeScene);
