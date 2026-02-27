f6f3adfd296c7002e2d21afffb2fabfb0b97929f
Georges <georges.boughantous@gmail.com>
2026-02-27 03:23:34 +0000

    lab2

--- labs/lab02/build.js
+++ labs/lab02/build.js
@@ -29,7 +29,7 @@ var EARTH_ORBIT_FACTOR = 3; // Earth orbit radius factor (multiplier of Earth ra
 var MOON_ORBIT_FACTOR = 2; // Moon orbit radius factor (multiplier of Earth radius)
 var earthRadius = SUN_RADIUS * EARTH_RATIO; // Earth radius (from constant)
 var moonRadius = earthRadius * MOON_RATIO; // Moon radius (from constant)
-var earthOrbit = SUN_RADIUS * EARTH_ORBIT_FACTOR; // Earth orbit radius (from formula)
+var earthOrbit = 8+SUN_RADIUS * EARTH_ORBIT_FACTOR; // Earth orbit radius (from formula)
 var moonOrbit = earthRadius * MOON_ORBIT_FACTOR; // Moon orbit radius (from formula)
     
 /* Define the build system function
--- labs/lab02/setup.js
+++ labs/lab02/setup.js
@@ -16,7 +16,7 @@ function setScene() {
     scene = new THREE.Scene(); // create a new empty 3D scene
     var ratio = window.innerWidth / window.innerHeight; // calculate screen aspect ratio
     camera = new THREE.PerspectiveCamera(45, ratio, 0.1, 1000); // create perspective camera with FOV and clipping planes
-    camera.position.set(10, 10, 30); // position camera so objects are visible
+    camera.position.set(10, 10, 350); // position camera so objects are visible
     camera.lookAt(0, 0, 0); // aim camera toward scene center
     renderer = new THREE.WebGLRenderer({ antialias: true }); // create renderer using GPU with smoother edges
     renderer.setPixelRatio(window.devicePixelRatio); // improve clarity on high-resolution screens
@@ -29,6 +29,7 @@ function setScene() {
     controls.maxDistance = 50; // prevent zooming too far
     controls.enableDamping = true; // enable smooth camera inertia motion
     controls.dampingFactor = 0.08; // control how quickly camera movement slows
+    controls.enablePan = false; // disable camera panning (left/right/up/down)
    
 }
 
